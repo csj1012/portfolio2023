48852901c02d11d71dd5835819c1d8f4013b2942
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-09 10:38:08 -0600

    Resolve merge conflict.

--- src/views/components/pages/RootPage.jsx
+++ src/views/components/pages/RootPage.jsx
@@ -1,9 +1,7 @@
-import { Layout, Header, Nav, Main, Aside, Footer } from '@components/templates/Layout'
+import { Layout, Header, Nav, Main, Footer } from '@components/templates/Layout'
 import MainMenu from '@components/organisms/MainMenu'
+import Masthead from '@components/organisms/Masthead'
 import { useLocation } from 'react-router-dom'
-import Masthead from '@components/molecules/Masthead'
-import PullQuote from '../molecules/PullQuote'
-
 
 export default function RootPage({ outlet }) {
   const { pathname } = useLocation()
@@ -17,20 +15,6 @@ export default function RootPage({ outlet }) {
         <Masthead />       
       </Header>
       <Main>{ outlet }</Main>
-      <div style={{ display: 'none' }}>
-          <div className='bg-moss h-24 w-24'>test</div>
-          <div className='bg-licorice h-24 w-24'>test</div>
-          <div className='bg-blackbean h-24 w-24'>test</div>
-          <div className='bg-floralwhite h-24 w-24'>test</div>
-          <div className='bg-licorice h-24 w-24'>test</div>
-          <div className='bg-tawny h-24 w-24'>test</div>
-          <div className='bg-yellow h-24 w-24'>test</div>
-          <div className='bg-citrine h-24 w-24'>test</div>
-          <div className='bg-frenchgray h-24 w-24'>test</div>
-        </div>
-      <Aside>
-        <PullQuote />
-      </Aside>
       { pathname !== '/' && (
         <Footer>
           <MainMenu />

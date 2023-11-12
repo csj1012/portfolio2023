d21e31d214417f2483760e382aa1c3730ea7efb6
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-11 18:46:05 -0600

    Update config and npm dependency vulnerabilities
    - Remove portfolio2023 directory from base path in vite.config.js
    - Update packages

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '/portfolio2023/',
+export default defineConfig({  
   plugins: [react()],
   resolve: {
     alias: {

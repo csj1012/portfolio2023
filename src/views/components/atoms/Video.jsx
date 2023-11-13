922279cb0f8da9f98ee4561b964020a126ef3b8b
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-13 14:06:09 -0600

    Don't autoplay if prefers-reduced-motion is enabled.

--- src/views/components/atoms/Video.jsx
+++ src/views/components/atoms/Video.jsx
@@ -1,7 +1,9 @@
 import ReactPlayer from 'react-player'
 import classNames from 'classnames'
+import usePrefersReducedMotion from '@components/util/prefersReducedMotion'
 
 export default function Video({ src, ...props } ) {
+  console.log(usePrefersReducedMotion())
   const { className } = props
   const classes = className ? classNames('react-player', className) : 'react-player'
   return (
@@ -9,7 +11,7 @@ export default function Video({ src, ...props } ) {
       <ReactPlayer
         className={classes}
         url={src}
-        playing={true}
+        playing={usePrefersReducedMotion() ? false : true}
         muted={true}
         width='100%'
         height='100%'        
--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
       '@controllers': path.resolve('/src/controllers'),
       '@routes': path.resolve('/src/controllers/routes'),
       '@views': path.resolve('/src/views'),
-      '@components': path.resolve('/src/views/components')
+      '@components': path.resolve('/src/views/components'),
     }
   }
 })

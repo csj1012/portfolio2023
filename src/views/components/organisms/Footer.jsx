4a6070de387382a5ea15fbbc128f57d357cae6f0
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-14 12:14:14 -0600

    Adjust footer styling.

--- src/views/components/organisms/Footer.jsx
+++ src/views/components/organisms/Footer.jsx
@@ -1,9 +1,9 @@
 import heart from '/assets/svg/heart.svg'
 
 export default function Footer() {
-  const Heart = <img src={heart} alt='' className='w-[12px] fill-root/50 inline-block mb-[3px]'/>
+  const Heart = <img src={heart} alt='' className='footer__heart'/>
   const year = new Date().getFullYear()
-  return <div className="footer-wrapper  tracking-wide h-full flex font-sans text-xs align-center justify-center bg-hazelight text-root/75 border-t-2 border-haze">
-    <p className='m-2  text-xs'>&#169; <span className='font-sans text-xs'>Chelsie Johnston,</span> {year}. <span className='font-sans text-xs'>Made with {Heart}.</span></p>
+  return <div className="footer-wrapper">
+    <p className='m-2 text-xs'>&#169; <span className='font-sans text-xs'>Chelsie Johnston, {year}. Made with {Heart}.</span></p>
   </div>
 }

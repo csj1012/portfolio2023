475b9fd27ce67e1c4392463bb33b288468ec9f7a
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-24 14:21:31 -0600

    Zazz it up with extra zazz.

--- src/views/components/organisms/Intro.jsx
+++ src/views/components/organisms/Intro.jsx
@@ -11,7 +11,7 @@ export default function Intro() {
         <p className="intro__caption">&#34;Chelsie and Maurice, Art Nouveau&#34; by Zach Henderson</p>
       </aside>
       <article className="intro__copy">
-        <p className="intro__copy--special">Hello! I&#39;m a developer with <em>a love for front end</em>. A little about  me:</p>
+        <p className="intro__copy--special">Hello! I&#39;m Chelsie, a developer with a love for front end. A little about  me:</p>
         <p>My journey through code & design has been shaped by the continuous joy I&#39;ve found in crafting experiences that evoke emotion and foster a sense of connection—a passion kindled from an early start with coding at age 10.</p>
         <p>Throughout my time as a developer I’ve cultivated a deep appreciation for design systems for their ability to marry visual and functional coherence. </p>
         <p>On the human side of things, I’m very interested in how project teams bridge the gap between UX, visual design, and front end development. I’m always curious about how project teams can work best together (an area of interest which combines agile project management methodology, team psychology, and design and development workflow.</p>

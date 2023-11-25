3082458741d1d394a5292755ceb008b9899a6461
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-24 21:03:37 -0600

    Style contact section.

--- src/views/components/pages/Contact.jsx
+++ src/views/components/pages/Contact.jsx
@@ -2,21 +2,26 @@ import { Link } from 'react-router-dom'
 import Image from '@components/atoms/Image'
 import png from '/assets/images/dill.png'
 import webp from '/assets/images/dill.webp'
+import Github from '/assets/github.png'
+import Linkedin from '/assets/linkedin.png'
+import Email from '/assets/svg/email.png'
 
 export default function Contact() {
   return (
-    <section className='contact'>    
-      <div className='contact__info'>
-        <p>Let's chat:</p>
-        <ul>
-          <li>LinkedIn: <Link to='https://www.linkedin.com/in/chelsiesjohnston/'>chelsiesjohnston</Link></li>
-          <li>GitHub: <Link to='https://github.com/csj1012'>csj1012</Link></li>
-          <li>Email: <Link to='mailto:chelsiesjohnston@gmail.com'>chelsiesjohnston@gmail.com</Link></li>
-        </ul>
-      </div>
-      <aside className="contact__art">
-        <Image src={png} webp={webp} dimensions={{'height': 680, 'width': 680}} alt='Chelsie and a dill plant.' />
-      </aside>      
+    <section className='contact'>
+      <div className='inset'>
+        <div className='contact__info'>
+          <p>Let's chat:</p>
+          <ul>
+            <li><span style={{ 'backgroundImage': `url(${Linkedin})`}} alt='Linkedin logo' /> <Link to='https://www.linkedin.com/in/chelsiesjohnston/'>chelsiesjohnston</Link></li>
+            <li><span style={{ 'backgroundImage': `url(${Github})`}} alt='Github logo' /> <Link to='https://github.com/csj1012'>csj1012</Link></li>
+            <li><span style={{ 'backgroundImage': `url(${Email})`}} alt='Email icon' /> <Link to='mailto:chelsiesjohnston@gmail.com'>chelsiesjohnston@gmail.com</Link></li>
+          </ul>
+        </div>
+        <aside className="contact__art">
+          <Image src={png} webp={webp} dimensions={{'height': 680, 'width': 680}} alt='Chelsie and a dill plant.' />
+        </aside>      
+      </div>    
     </section>
   )
 }

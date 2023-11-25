34656e1ce3c48099ef4987fd8156f32f9f181646
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2023-11-24 21:06:19 -0600

    Copy udpates.

--- src/views/components/organisms/WorkStyle.jsx
+++ src/views/components/organisms/WorkStyle.jsx
@@ -6,13 +6,13 @@ export default function WorkStyle() {
       <h2 className='workstyle__title'>How I Work</h2>
       <p className='workstyle__intro'>My favorite teams to be part of are ones made of people who consciously:</p>
       <ul>
-        <li>Lead by example (and team building exercises)</li>
+        <li>Lead by example</li>
         <li>Maintain clean, sensible markup and styling (using BEM, SMACSS, modular CSS—whichever methodology fits the project)</li>
-        <li>Streamline development and deployment workflow - from local to live - using automated testing and deployment tools</li>
+        <li>Streamline development and deployment workflow—from local to live—using automated testing and deployment tools</li>
         <li>Curate a well-organized, cohesive design pattern library</li>
         <li>Cultivate transparency and honesty in organizations, with clients, and especially among project teams</li>
         <li>Spend the time and energy to get the details right</li>
-        <li>Iterate upon nimble project methodologies that think outside the box</li>
+        <li>Iterate often using nimble project methodologies (thinking outside the box!)</li>
         <li>Solve problems at the root</li>
         <li>Use the right tools for the job (next time, I&#39;ll probably build this in Astro...)</li>
         <li>Do what works and improve what doesn&#39;t</li>

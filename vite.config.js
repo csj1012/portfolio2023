3dcf3969fb95ef8584c349a9e767ba7fb0766fad
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2024-01-03 21:46:43 -0600

    Remove project directory from vite config.

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({  
   plugins: [react()],
-  base: '/portfolio2023/',
   resolve: {
     alias: {
       '@models': path.resolve('src/models'),

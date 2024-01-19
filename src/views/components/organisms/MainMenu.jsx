cf500ea126d8b79c77f94e4ce623941954db91cd
Chelsie Johnston <39135832+csj1012@users.noreply.github.com>
2024-01-19 14:19:09 -0600

    Use SVG for main menu background texture.

--- src/views/components/organisms/MainMenu.jsx
+++ src/views/components/organisms/MainMenu.jsx
@@ -1,5 +1,7 @@
 import { Link, NavLink } from 'react-router-dom'
-import bgImage from '/assets/images/nouveau-bg.png'
+import bgImage from '/assets/svg/noise.svg'
+import fgImage from '/assets/svg/noiseOverlay.svg'
+import textImage from '/assets/svg/noiseDark.svg'
 
 export default function MainMenu() {
   const items = [    
@@ -25,9 +27,9 @@ export default function MainMenu() {
   )
   
   return (
-    <menu className="main-menu" style={{backgroundImage: `url(${bgImage})`}}>
+    <menu className="main-menu" style={{backgroundImage: `url(${fgImage}), url(${bgImage})`}}>    
       <h1 className="main-menu__title">        
-        <Link to="/" className='main-menu__title__link'>Chelsie Johnston</Link>        
+        <Link to="/" className='main-menu__title__link'  style={{backgroundImage: `url(${textImage})`}}>Chelsie Johnston</Link>        
       </h1>
       { < HeadingElement /> } 
       <ul className='main-menu__menu bg-cover'>{menuItems}</ul>
@@ -35,4 +37,3 @@ export default function MainMenu() {
   )
   
 }
-
